28648436305cd695ef2c9879233a8241f1619409
Yash Singh <ys181544@gmail.com>
2026-01-01 18:23:44 +0530

    new

--- second-project/src/main/java/com/example/second_project/controller/PageController.java
+++ second-project/src/main/java/com/example/second_project/controller/PageController.java
@@ -1,5 +1,8 @@
 package com.example.second_project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,17 @@ public class PageController {
         //if condition
 
         model.addAttribute("ishero", false);
+        //loop
+
+      List<String>avengers = new ArrayList<>();
+      avengers.add("Captain America");
+      avengers.add("Hulk");
+      avengers.add("Spider man");
+      avengers.add("Thor");
+      avengers.add("Iron man");
+
+      model.addAttribute("aven", avengers);
+
         return "index";
     }
       @GetMapping("/about")
